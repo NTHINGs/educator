0ef65f7029856d90ef974588631d5ee3f0a91396
Mauricio Martinez <n_othing@hotmail.com>
2018-08-22 16:11:02 -0500

    Bootstrap

--- templates/content-course.php
+++ templates/content-course.php
@@ -14,11 +14,11 @@ $thumb_size = apply_filters( 'edr_courses_thumb_size', 'thumbnail' );
 <article id="course-<?php echo intval( $course_id ); ?>" class="col-12 col-sm-4 edr-course">
 	<a href="<?php the_permalink(); ?>">
 		<div class="card">
-			<div class="card-img-top" style="height=300px;">
+			<div class="card-img-top" style="height:300px;">
 				<?php if ( has_post_thumbnail() ) :
-					the_post_thumbnail('full');
+					the_post_thumbnail('full', array('class' => 'img-fluid'));
 				else:
-					echo '<img src="/wp-content/plugins/educator/assets/public/img/default-thumbnail.jpg">';
+					echo '<img class="img-fluid" src="/wp-content/plugins/educator/assets/public/img/default-thumbnail.jpg">';
 				endif; ?>
 			</div>
 			<div class="card-body">
