d6fafcd3d906b2cbbf3345cd6feff37045c366ab
Mauricio Martinez <n_othing@hotmail.com>
2018-08-22 16:30:53 -0500

    Bootstrap

--- templates/content-course.php
+++ templates/content-course.php
@@ -13,20 +13,18 @@ $thumb_size = apply_filters( 'edr_courses_thumb_size', 'thumbnail' );
 ?>
 <article id="course-<?php echo intval( $course_id ); ?>" class="col-12 col-sm-4 edr-course">
 	<a href="<?php the_permalink(); ?>">
-		<div class="card h-100">
-			<div class="card-img-top text-center h-50">
-				<?php if ( has_post_thumbnail() ) :
-					the_post_thumbnail('full' , array('class' => 'img-fluid'));
-				else:
-					echo '<img class="img-fluid wp-post-image" src="/wp-content/plugins/educator/assets/public/img/default-thumbnail.jpg">';
-				endif; ?>
-			</div>
-			<div class="card-body h-50">
+		<div class="card">
+			<?php if ( has_post_thumbnail() ) :
+				the_post_thumbnail('full' , array('class' => 'img-fluid card-img-top'));
+			else:
+				echo '<img class="img-fluid wp-post-image card-img-top" src="/wp-content/plugins/educator/assets/public/img/default-thumbnail.jpg">';
+			endif; ?>
+			<div class="card-body">
 				<h5 class="card-title"><?php the_title(); ?></h5>
 				<p class="card-text"><?php the_excerpt(); ?></p>
 				<p class="float-right">
 					<?php
-						echo _x( 'Price:', 'Price str', 'novolearn' );
+						echo _x( 'Price: ', 'Price str', 'novolearn' );
 						echo $price_str; 
 					?>
 				</p>
