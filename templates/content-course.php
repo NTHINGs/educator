ab8b602a61f9fed3faa6a32c0494fd6e0027ea2e
Mauricio Martinez <n_othing@hotmail.com>
2018-08-22 17:03:13 -0500

    Bootstrap

--- templates/content-course.php
+++ templates/content-course.php
@@ -11,7 +11,7 @@ $price = $edr_courses->get_course_price( $course_id );
 $price_str = ( $price > 0 ) ? edr_format_price( $price ) : _x( 'Free', 'price', 'novolearn' );
 $thumb_size = apply_filters( 'edr_courses_thumb_size', 'thumbnail' );
 ?>
-<article id="course-<?php echo intval( $course_id ); ?>" class="col-12 col-sm-4 edr-course">
+<article id="course-<?php echo intval( $course_id ); ?>" class="col-12 col-sm-6 col-md-4 edr-course">
 	<a href="<?php the_permalink(); ?>">
 		<div class="card h-100">
 			<?php if ( has_post_thumbnail() ) :
