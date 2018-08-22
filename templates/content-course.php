98021a6e3a03308e2d1f0114d2e166c4ffe156c1
Mauricio Martinez <n_othing@hotmail.com>
2018-08-22 16:43:17 -0500

    Bootstrap

--- templates/content-course.php
+++ templates/content-course.php
@@ -13,7 +13,7 @@ $thumb_size = apply_filters( 'edr_courses_thumb_size', 'thumbnail' );
 ?>
 <article id="course-<?php echo intval( $course_id ); ?>" class="col-12 col-sm-4 edr-course">
 	<a href="<?php the_permalink(); ?>">
-		<div class="card">
+		<div class="card h-100">
 			<?php if ( has_post_thumbnail() ) :
 				the_post_thumbnail('large' , array('class' => 'img-fluid card-img-top'));
 			else:
